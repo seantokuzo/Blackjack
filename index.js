e546852e891b168899afa16054e6c80eba2e6fd7
CrackerJaps <seantokuzo@gmail.com>
2022-02-11 16:58:55 -0800

    API call, local storage of deckId and display cards done

--- index.js
+++ index.js
@@ -1,8 +1,105 @@
-// let firstCard = 6
-// let secondCard = 9
-// let sum = firstCard + secondCard
-// let hasBlackJack = false
-// isAlive = true
+const myButton = document.getElementById('button')
+const firstCard = document.getElementById("card1")
+const secondCard = document.getElementById("card2")
+const thirdCard = document.getElementById("card3")
+const fourthCard = document.getElementById("card4")
+const fifthCard = document.getElementById("card5")
+let currentDeck = ''
+let myStorage = window.localStorage
+let drawCount = 0
+
+const setCard = (card, url) => {
+    card.src = url
+    card.style.zIndex = 2
+    card.style.visibility = "visible"
+}
+
+const resetCard = (card) => {
+    card.src = ''
+    card.style.zIndex = -1
+    card.style.visibility = "hidden"
+}
+
+const getYourDeck = () => {
+    if (myStorage.deckId) {
+        currentDeck = myStorage.getItem('deckId')
+        console.log(currentDeck)
+    } else {
+        fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=2")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                myStorage.setItem('deckId', data.deck_id)
+                console.log(myStorage)
+                setCard(firstCard, data.cards[0].image)
+                setCard(secondCard, data.cards[1].image)
+                currentDeck = data.deck_id
+                drawCount = 2
+            })
+            .catch(e => console.log(e))
+    }
+}
+
+getYourDeck()
+
+function Draw() {
+    if (drawCount === 0) {
+        fetch(`https://deckofcardsapi.com/api/deck/${currentDeck}/shuffle/`)
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(e => console.log(e))
+        fetch(`https://deckofcardsapi.com/api/deck/${currentDeck}/draw/?count=2`)
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data)
+                myStorage.setItem('deckId', data.deck_id)
+                setCard(firstCard, data.cards[0].image)
+                setCard(secondCard, data.cards[1].image)
+                drawCount += 2
+                currentDeck = data.deck_id
+            })
+            .catch(e => console.log(e))
+    } else if (drawCount === 4) {
+        fetch(`https://deckofcardsapi.com/api/deck/${currentDeck}/draw/?count=1`)
+            .then(res => res.json())
+            .then(data => {
+                const currentCard = document.getElementById(`card${drawCount + 1}`)
+                setCard(currentCard, data.cards[0].image)
+                drawCount += 1
+                myButton.innerText = "New Game"
+            })
+            .catch(e => console.log(e))
+    } else if (drawCount > 0 && drawCount < 4) {
+        fetch(`https://deckofcardsapi.com/api/deck/${currentDeck}/draw/?count=1`)
+            .then(res => res.json())
+            .then(data => {
+                const currentCard = document.getElementById(`card${drawCount + 1}`)
+                setCard(currentCard, data.cards[0].image)
+                drawCount += 1
+            })
+            .catch(e => console.log(e))
+    } else {
+        drawCount = 0
+        resetCard(thirdCard)
+        resetCard(fourthCard)
+        resetCard(fifthCard)
+        fetch(`https://deckofcardsapi.com/api/deck/${currentDeck}/shuffle/`)
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(e => console.log(e))
+        fetch(`https://deckofcardsapi.com/api/deck/${currentDeck}/draw/?count=2`)
+            .then(res => res.json())
+            .then(data => {
+                setCard(firstCard, data.cards[0].image)
+                setCard(secondCard, data.cards[1].image)
+                drawCount = 2
+                myButton.innerText = "Draw"
+            })
+            .catch(e => console.log(e))
+    }
+}
+
+
 
 // if (sum <= 20) {
 //     console.log("Want to draw another card? 🙂")
@@ -11,33 +108,3 @@
 // } else {
 //     console.log("Busted! 😭")
 // }
-
-let deckId = ''
-let shuffled = false
-
-function getNewDeck(id) {
-    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            deckId = data.deck_id
-            console.log(deckId)
-            shuffled = data.shuffled
-            console.log(shuffled)
-        })
-        .catch(e => console.log(e))
-}
-
-getNewDeck(deckId)
-
-// function firstDraw(id) {
-//     fetch(`https://deckofcardsapi.com/api/deck/${id}/draw/?count=2`)
-//         .then(res => {
-//             console.log(deckId)
-//             res.json()
-//         })
-//         .then(data => console.log(data))
-//         .catch(e => console.log(e))
-// }
-
-// firstDraw(deckId)
